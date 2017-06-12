182897e530d8ab4c7e39cb9e15c2eb2d7443d2cc
Felipe Santibañez-Leal <cuentasfsantibanez@gmail.com>
2017-06-12 16:36:43 -0400

    Updating calls

--- Twin Arrays/Attemp1.py
+++ Twin Arrays/Attemp1.py
@@ -3,14 +3,13 @@
 import sys
 
 def twinArrays(ar1, ar2):
-    outTwin = 0
     # Complete this function
     ar1ST = sorted((e,i) for i,e in enumerate(ar1))
     ar2ST = sorted((e,i) for i,e in enumerate(ar2))
-    
-    if ar1ST[0][1] != ar2ST[0][1]:
-        outTwin = ar1ST[0][0] + ar2ST[0][0]
-    else:
+
+    outTwin = ar1ST[0][0] + ar2ST[0][0]
+
+    if ar1ST[0][1] == ar2ST[0][1]:
         outTwin = min(ar1ST[0][0] + ar2ST[1][0], ar1ST[1][0] + ar2ST[0][0])
     return outTwin
     
@@ -18,5 +17,4 @@ n = int(input().strip())
 ar1 = list(map(int, input().strip().split(' ')))
 ar2 = list(map(int, input().strip().split(' ')))
 
-result = twinArrays(ar1, ar2)
-print(result)
+print(twinArrays(ar1, ar2))
